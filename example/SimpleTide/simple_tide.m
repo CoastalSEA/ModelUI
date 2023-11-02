ddf3698570b6fc9ab0c8f1f70785e88938e50b4f
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2023-11-02 09:23:39 +0000

    Asmita ASM_Plots - corrected metadata string used for Display name to include Element name. Element - removed reset of scaling for beach type elements. Not needed as long as equilibrium area is based on initial area. muiAppCoastalFcns Corrected error in simple_tide where speed of wave was being doubled. Tidied up documentation. This applies to simple_tide in ModelUI as well. Added classes and functions to SedTools to compute total load sediment transport. Started to update manual.
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/ModelUI@197 5305ae0c-0cd9-424f-b575-fb45b188e99a

--- example/SimpleTide/simple_tide.m
+++ example/SimpleTide/simple_tide.m
@@ -14,12 +14,12 @@ function tide = simple_tide(inp)
 %         Tinterval                 time interval for simulation (mins)
 %         z0                        mean tidel level to ordnance datum (mOD)
 %         TidalAmp                  tidal elevation amplitude (m)
-%         ElevPhase                 phase of elevation (ie k.x) (rads)
+%         ElevPhase                 phase of elevation (ie k.x) (deg)
 %         VelocityAmp               tidal velocity amplitude (m/s)
-%         VelocityPhase             phase of velocity (ie k.x+phi) (rads)
-%         aM2                       M2 tidal amplitufe (m)
-%         aS2                       S2 tidal amplitufe (m)
-%         aO1                       O1 tidal amplitufe (m)
+%         VelocityPhase             phase of velocity (ie k.x+phi) (deg)
+%         aM2                       M2 tidal amplitude (m)
+%         aS2                       S2 tidal amplitude (m)
+%         aO1                       O1 tidal amplitude (m)
 % OUTPUTS
 %   tide - structure for time series containing
 %        > time at Tintervals for Duration
@@ -28,40 +28,40 @@ function tide = simple_tide(inp)
 %        > horizointal velocity (m/s)
 %
 % Author: Ian Townend
-% CoastalSEA (c)June 2020
+% CoastalSEA (c)June 2020, corrected Nov 2023
 %--------------------------------------------------------------------------
 %
-Dur  = inp.Duration*24*3600;       %duration (seconds)
-tint = inp.Tinterval*60;           %time interrval for simulation (secs)
-z0   = inp.z0;
-amp  = inp.TidalAmp;
-pha1 = inp.ElevPhase*pi/180;
-U    = inp.VelocityAmp;
-pha2 = inp.VelocityPhase*pi/180;
-aM2  = inp.aM2;        %M2 tidal amplitufe (m)
-aS2  = inp.aS2;        %S2 tidal amplitufe (m)
-aO1  = inp.aO1;        %O1 tidal amplitufe (m)
-
-fact = amp/(aM2+aS2+aO1);
-aM2s = fact*aM2;       %scaled amplitude of M2 constituent
-wM2  = 1.405e-4;       %angular speed of M2 constituent (rads/s)
-aS2s = fact*aS2;       %scaled amplitude of S2 constituent
-wS2  = 1.455e-4;       %angular speed of S2 constituent (rads/s)
-aO1s = fact*aO1;       %scaled amplitude of O1 constituent
-wO1  = wM2/2;          %angular speed of O1 constituent (rads/s)
-
-tide.t = (0:tint:Dur)';
-tide.z = aM2s*cos(2*wM2*tide.t+pha1)+...
-                 aS2s*cos(2*wS2*tide.t+pha1)+...
-                 aO1s*cos(2*wO1*tide.t+pha1)+z0;
-
-tide.dz = -2*wM2*aM2s*sin(2*wM2*tide.t+pha1)...
-                -2*wS2*aS2s*sin(2*wS2*tide.t+pha1)...
-                -2*wO1*aO1s*sin(2*wO1*tide.t+pha1);
-
-tide.u = -(aM2s*sin(2*wM2*tide.t+pha2)+...
-                 aS2s*sin(2*wS2*tide.t+pha2)+...
-                 aO1s*sin(2*wO1*tide.t+pha2))*U/amp;
+    Dur  = inp.Duration*24*3600;       %duration (seconds)
+    tint = inp.Tinterval*60;           %time interval for simulation (secs)
+    z0   = inp.z0;
+    amp  = inp.TidalAmp;
+    pha1 = inp.ElevPhase*pi/180;
+    U    = inp.VelocityAmp;
+    pha2 = inp.VelocityPhase*pi/180;
+    aM2  = inp.aM2;        %M2 tidal amplitude (m)
+    aS2  = inp.aS2;        %S2 tidal amplitude (m)
+    aO1  = inp.aO1;        %O1 tidal amplitude (m)
+    
+    fact = amp/(aM2+aS2+aO1);
+    aM2s = fact*aM2;       %scaled amplitude of M2 constituent
+    wM2  = 1.405e-4;       %angular speed of M2 constituent (rads/s)
+    aS2s = fact*aS2;       %scaled amplitude of S2 constituent
+    wS2  = 1.455e-4;       %angular speed of S2 constituent (rads/s)
+    aO1s = fact*aO1;       %scaled amplitude of O1 constituent
+    wO1  = wM2/2;          %angular speed of O1 constituent (rads/s)
+    
+    tide.t = (0:tint:Dur)';
+    tide.z = aM2s*cos(wM2*tide.t-pha1)+...
+                     aS2s*cos(wS2*tide.t-pha1)+...
+                     aO1s*cos(wO1*tide.t-pha1)+z0;
+    
+    tide.dz = -wM2*aM2s*sin(wM2*tide.t-pha1)...
+                    -wS2*aS2s*sin(wS2*tide.t-pha1)...
+                    -wO1*aO1s*sin(wO1*tide.t-pha1);
+    
+    tide.u = -(aM2s*sin(wM2*tide.t-pha2)+...
+                     aS2s*sin(wS2*tide.t-pha2)+...
+                     aO1s*sin(wO1*tide.t-pha2))*U/amp;
 end
 
 
