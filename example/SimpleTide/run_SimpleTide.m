9918cafea31c325eb1ef15b5187008600124c870
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2024-01-21 17:21:20 +0000

    CSThydraulics - moved check for CSTmodel to class constructor ckfa_form_solver - corrected error in estimation of convergence length cf_changeplot, cf_sectionplot, cf_summaryplot - added text to output, improved dsiplay information and added mouth location to some plots. CSTmodel - Fixed bug to plot stand-alone figure from xPlot tab EnergyFlux - Implemented XY plotting on tab CoastalFcns- tau_bed - added Cd to ouput. Corrected help notes in ucrit. Added option to pass input struct to settling_velocity.m. EstuaryFcns- Added option to pass input struct to river_regime.m.
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/ModelUI@214 5305ae0c-0cd9-424f-b575-fb45b188e99a

--- example/SimpleTide/run_SimpleTide.m
+++ example/SimpleTide/run_SimpleTide.m
@@ -1,7 +1,7 @@
 function st = run_SimpleTide
     %intialise environment settings to run SimpleTide
     muimsg = 'ModelUI not found. Check that App is installed';
-    ok = initialise_mui_app('ModelUI',muimsg,'example');
-    if ok<1, return; end
+    isok = initialise_mui_app('ModelUI',muimsg,'example');
+    if ~isok, return; end
     st = SimpleTide;
 end
